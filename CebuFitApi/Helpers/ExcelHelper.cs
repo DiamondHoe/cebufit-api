78e9f53651c331055b5f25100fc5f43643dc83ce
Robson <bobson50364@gmail.com>
2024-02-12 22:40:50 +0100

    summary products

--- CebuFitApi/Helpers/ExcelHelper.cs
+++ CebuFitApi/Helpers/ExcelHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CebuFitApi.Interfaces;
 using CebuFitApi.Models;
@@ -16,16 +18,37 @@ namespace CebuFitApi.Helpers
             AddHeaders(worksheet);
 
             int row = 2;
+            Dictionary<Guid, double> ingredientSum = new Dictionary<Guid, double>(); // Dictionary to store summed quantities by base product ID
+
             foreach (var day in days)
             {
                 foreach (var meal in day.Meals)
                 {
                     foreach (var ingredient in meal.Ingredients)
                     {
-                        FillRow(worksheet, row, ingredient);
-                        row++;
+                        // Check if the base product ID exists in the dictionary
+                        if (ingredientSum.ContainsKey(ingredient.Product.Id))
+                        {
+                            // If yes, add the quantity or weight to the existing sum
+                            ingredientSum[ingredient.Product.Id] += (double)(ingredient.Quantity ?? ingredient.Weight ?? 0);
+                        }
+                        else
+                        {
+                            // If no, initialize the sum with the current quantity or weight
+                            ingredientSum[ingredient.Product.Id] = (double)(ingredient.Quantity ?? ingredient.Weight ?? 0);
+                        }
                     }
                 }
+            }
+
+            // Write rows with summed quantities
+            foreach (var (productId, sum) in ingredientSum)
+            {
+                var ingredient = days.SelectMany(day => day.Meals.SelectMany(meal => meal.Ingredients))
+                    .FirstOrDefault(ing => ing.Product.Id == productId);
+
+                FillRow(worksheet, row, ingredient, sum);
+                row++;
             }
 
             worksheet.Cells.AutoFitColumns();
@@ -41,26 +64,15 @@ namespace CebuFitApi.Helpers
             worksheet.Cells[1, 5].Value = "Category";
         }
 
-        private void FillRow(ExcelWorksheet worksheet, int row, Ingredient ingredient)
+        private void FillRow(ExcelWorksheet worksheet, int row, Ingredient ingredient, double sum)
         {
             worksheet.Cells[row, 1].Value = ingredient.Product.Name;
             worksheet.Cells[row, 3].Value = ingredient.Product.Name;
             worksheet.Cells[row, 4].Value = ingredient.Product.Importance;
             worksheet.Cells[row, 5].Value = ingredient.Product.Category?.Name;
 
-            // Check if Quantity is null, then use Weight; vice versa
-            if (ingredient.Quantity != null)
-            {
-                worksheet.Cells[row, 2].Value = ingredient.Quantity;
-            }
-            else if (ingredient.Weight != null)
-            {
-                worksheet.Cells[row, 2].Value = ingredient.Weight;
-            }
-            else
-            {
-                worksheet.Cells[row, 2].Value = ""; // Handle case when both Quantity and Weight are null
-            }
+            // Set the summed quantity/weight
+            worksheet.Cells[row, 2].Value = sum;
         }
     }
 }
