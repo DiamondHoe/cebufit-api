c768656b4dcbeb48768c61e641c677b14ec2099f
DiamondHoe <desineverbum@gmail.com>
2024-02-15 20:18:23 +0100

    migration

---
+++ CebuFitApi/Migrations/20240215191756_storageItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuFitApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class storageItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Eaten",
+                table: "StorageItems",
+                newName: "Prepared");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Prepared",
+                table: "StorageItems",
+                newName: "Eaten");
+        }
+    }
+}
