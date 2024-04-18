c4294337f515c242819c4877265ecacbc897e92e
DiamondHoe <desineverbum@gmail.com>
2024-04-18 21:12:52 +0200

    migracja

---
+++ CebuFitApi/Migrations/20240418191225_storageItemAdjust.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuFitApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class storageItemAdjust : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prepared",
+                table: "StorageItems");
+
+            migrationBuilder.RenameColumn(
+                name: "Weight",
+                table: "StorageItems",
+                newName: "BoughtWeight");
+
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "StorageItems",
+                newName: "BoughtQuantity");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ActualQuantity",
+                table: "StorageItems",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ActualWeight",
+                table: "StorageItems",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActualQuantity",
+                table: "StorageItems");
+
+            migrationBuilder.DropColumn(
+                name: "ActualWeight",
+                table: "StorageItems");
+
+            migrationBuilder.RenameColumn(
+                name: "BoughtWeight",
+                table: "StorageItems",
+                newName: "Weight");
+
+            migrationBuilder.RenameColumn(
+                name: "BoughtQuantity",
+                table: "StorageItems",
+                newName: "Quantity");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Prepared",
+                table: "StorageItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
