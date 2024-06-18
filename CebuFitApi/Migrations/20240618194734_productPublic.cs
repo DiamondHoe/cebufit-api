c551b478fd1b38855ecd7876fec110dcdda41f3f
DiamondHoe <desineverbum@gmail.com>
2024-06-18 21:48:02 +0200

    migration added

---
+++ CebuFitApi/Migrations/20240618194734_productPublic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CebuFitApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class productPublic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Product",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Product");
+        }
+    }
+}
