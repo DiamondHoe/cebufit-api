b18d69397159a803c2eff8e9a02c042e4eb171a7
Robson <bobson50364@gmail.com>
2024-11-14 17:45:40 +0100

    clear dto

--- CebuFitApi/DTOs/Request/RequestRecipeWithDetailsDTO.cs
+++ CebuFitApi/DTOs/Request/RequestRecipeWithDetailsDTO.cs
@@ -2,13 +2,7 @@
 {
     public class RequestRecipeWithDetailsDto : RequestWithDetailsDto
     {
-        // public Guid Id { get; set; }
-        // public RequestType Type { get; set; }
-        // public UserPublicDto Requester { get; set; }
         public RecipeWithDetailsDTO RequestedRecipe { get; set; }
-        // public string Status { get; set; }
-        // public UserPublicDto? Approver { get; set; }
-        // public string? Description { get; set; }
     }
 }
 
