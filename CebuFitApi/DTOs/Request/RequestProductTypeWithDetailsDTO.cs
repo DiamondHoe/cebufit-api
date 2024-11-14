c0df1afd02de0a128ddd26674e13643f784f0723
Robson <bobson50364@gmail.com>
2024-11-14 17:43:01 +0100

    Clear classes

--- CebuFitApi/DTOs/Request/RequestProductTypeWithDetailsDTO.cs
+++ CebuFitApi/DTOs/Request/RequestProductTypeWithDetailsDTO.cs
@@ -2,13 +2,7 @@
 {
     public class RequestProductTypeWithDetailsDto : RequestWithDetailsDto
     {
-        // public Guid Id { get; set; }
-        // public RequestType Type { get; set; }
-        // public UserPublicDto Requester { get; set; }
         public ProductTypeDto RequestedProductType { get; set; }
-        // public string Status { get; set; }
-        // public UserPublicDto? Approver { get; set; }
-        // public string? Description { get; set; }
     }
 }
 
--- CebuFitApi/DTOs/Request/RequestProductWithDetailsDTO.cs
+++ CebuFitApi/DTOs/Request/RequestProductWithDetailsDTO.cs
@@ -2,13 +2,7 @@
 {
     public class RequestProductWithDetailsDto: RequestWithDetailsDto
     {
-        // public Guid Id { get; set; }
-        // public RequestType Type { get; set; }
-        // public UserPublicDto Requester { get; set; }
         public ProductWithDetailsDTO RequestedProduct { get; set; }
-        // public string Status { get; set; }
-        // public UserPublicDto? Approver { get; set; }
-        // public string? Description { get; set; }
     }
 }
 
