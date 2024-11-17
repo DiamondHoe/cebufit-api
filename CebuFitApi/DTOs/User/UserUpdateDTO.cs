a3902dbaabfd1842e441906be4e1e092d5475c67
DiamondHoe <desineverbum@gmail.com>
2024-11-17 13:21:40 +0100

    fix dto

--- CebuFitApi/DTOs/User/UserUpdateDTO.cs
+++ CebuFitApi/DTOs/User/UserUpdateDTO.cs
@@ -5,8 +5,6 @@ namespace CebuFitApi.DTOs.User
 {
     public class UserUpdateDTO
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
         public string Name { get; set; }
         public bool Gender { get; set; } //False = Man, True = Woman
         public int Height { get; set; }
